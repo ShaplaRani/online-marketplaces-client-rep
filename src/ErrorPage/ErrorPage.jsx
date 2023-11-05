8625c56073d3d2800cc60a4187e957b9aa19ef8d
ShaplaRani <shaplarani621@gmail.com>
2023-11-05 15:21:05 +0600

    added the error page in this website

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,5 @@
 import { Link, useRouteError } from "react-router-dom";
-
+import errorPic from '../assets/errorPic.jpg'
 
 const ErrorPage = () => {
     const error = useRouteError();
@@ -9,9 +9,12 @@ const ErrorPage = () => {
 
                 {
                     error.status === 404 && <div>
-                        <h3 className="text-lg mb-4">Page Not Found</h3>
-                        <Link to="/" className="bg-blue-600 text-white py-3 px-6 rounded-lg">
-                            Go Back</Link>
+                        <img className="w-1/3 mx-auto" src={errorPic} alt="" />
+                        {/* <h3 className="text-lg mb-4">Page Not Found</h3> */}
+                      <div className="  flex mt-6 justify-center">
+                      <button className="bg-emerald-400 text-white py-3 text-xl font-normal px-6 rounded-lg">  <Link to="/" >
+                        Back to home </Link></button>
+                      </div>
                     </div>
                 }
             </div>
